2ba8fe2bd242cab331c7522e7126718694f18e7c
Vishruth Raj <vishraj248@gmail.com>
2023-02-22 11:18:50 -0600

    Add mongo connection

--- api/server.py
+++ api/server.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import pymongo
+import private.credentials as creds
 
 app = Flask(__name__)
+
+
+client = pymongo.MongoClient("mongodb+srv://" + creds.username + ":<" + creds.password + ">@cluster0.vgbagff.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
 
 
 @app.route('/')
