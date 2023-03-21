bba24905f637badd29e76b3804ee4ecf581127df
Vishruth Raj <vishraj248@gmail.com>
2023-03-21 16:26:22 -0500

    Begin making model endpoint

--- api/server.py
+++ api/server.py
@@ -1,15 +1,29 @@
-from flask import Flask
-import pymongo
+from flask import Flask, request, jsonify
+# import pymongo
 import private.credentials as creds
+from helpers.model import PredictionModel
 
 app = Flask(__name__)
 
 
-client = pymongo.MongoClient("mongodb+srv://" + creds.username + ":<" + creds.password + ">@cluster0.vgbagff.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
+# client = pymongo.MongoClient("mongodb+srv://" + creds.username + ":<" + creds.password + ">@cluster0.vgbagff.mongodb.net/?retryWrites=true&w=majority")
+# db = client.test
 
-
+model = PredictionModel()
 
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # data = request.get_json(force=True)
+    # print(data)
+    data = request.json['cities']
+
+    # prediction = model.make_prediction(data)
+
+    return jsonify(data)
+
+    
+
